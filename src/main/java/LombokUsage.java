5ee0acb65c25aa63e85988546ce5dd291eec3ce5
Artyrio226 <artur_sar_80@mail.ru>
2024-02-21 09:29:25 +0300

    1. Lombok. Настройка IDEA [#504888 #482451]

---
+++ src/main/java/LombokUsage.java
@@ -0,0 +1,9 @@
+import ru.job4j.lombok.BirdData;
+
+public class LombokUsage {
+    public static void main(String[] args) {
+        var bird = new BirdData();
+        bird.setAge(1);
+        System.out.println(bird);
+    }
+}
