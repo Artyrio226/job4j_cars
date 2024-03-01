7896c07e9ff347d2d3b9fb5cfe6cc74870fea5b0
Artyrio226 <artur_sar_80@mail.ru>
2024-03-01 19:21:54 +0300

    1. ToMany [#301848] добавлен скрипт создания таблицы participates, создана модель PriceHistory, в Post добавлена связь ManyToMany

--- src/main/java/ru/job4j/cars/model/Post.java
+++ src/main/java/ru/job4j/cars/model/Post.java
@@ -15,16 +15,29 @@ import java.util.List;
 @Entity
 @Table(name = "auto_post")
 public class Post {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Include
     private int id;
+
     private String description;
+
     private LocalDateTime created;
+
     @ManyToOne
     @JoinColumn(name = "auto_user_id")
     private User user;
+
     @OneToMany(cascade = CascadeType.ALL)
     @JoinColumn(name = "auto_post_id")
     private List<PriceHistory> messengers = new ArrayList<>();
+
+    @ManyToMany
+    @JoinTable(
+            name = "participates",
+            joinColumns = { @JoinColumn(name = "post_id") },
+            inverseJoinColumns = { @JoinColumn(name = "user_id") }
+    )
+    private List<User> participates = new ArrayList<>();
 }
