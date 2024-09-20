44a805d91ad8ea6f016c62a316bd436e2ae8f883
Artyrio226 <artur_sar_80@mail.ru>
2024-09-20 20:43:38 +0300

    Поправил скрипт

--- src/main/resources/db/scripts/002_dml_insert_users.sql
+++ src/main/resources/db/scripts/002_dml_insert_users.sql
@@ -1,3 +1,5 @@
-INSERT INTO auto_user (login, password) VALUES ('Ivanov', 'root');
-INSERT INTO auto_user (login, password) VALUES ('Petrov', 'root');
-INSERT INTO auto_user (login, password) VALUES ('Sidorov', 'root');
+INSERT INTO auto_user (email, username, password, phone_number)
+VALUES
+    ('ivanov@mail.ru', 'Ivanov', 'root', '+79991112233'),
+    ('petrov@mail.ru', 'Petrov', 'root', '+79992223344'),
+    ('sidorov@mail.ru', 'Sidorov', 'root', '+79993334455');
