aad3071439b2cb4815c6868a85c31695c2bce4d5
Artyrio226 <artur_sar_80@mail.ru>
2024-09-21 20:56:46 +0300

    Поправил скрипт и hibernate конфигурации

--- src/main/resources/db/scripts/004_ddl_create_car.sql
+++ src/main/resources/db/scripts/004_ddl_create_car.sql
@@ -2,7 +2,7 @@ CREATE TABLE car
 (
     id BIGSERIAL PRIMARY KEY,
     name  TEXT NOT NULL ,
-    year INT NOT NULL ,
+    years INT NOT NULL ,
     mileage INT NOT NULL ,
     color  TEXT NOT NULL,
     body  TEXT NOT NULL,
